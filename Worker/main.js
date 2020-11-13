4a8158eefd4e79d0ad0e7125104885b5ca1e8929
Patrik Švikruha <patrik.svikruha@gmail.com>
2020-11-13 13:28:36 +0100

    format code

--- Worker/main.js
+++ Worker/main.js
@@ -1,11 +1,11 @@
 
 if (window.Worker) {
-    var myWorker = new Worker('worker.js');
-    
-    myWorker.onmessage = function (e) {
-        console.log(e.data);
-        console.log('Message received from worker');
-    }
+  var myWorker = new Worker('worker.js');
 
-    myWorker.postMessage(5);
+  myWorker.onmessage = function (e) {
+    console.log(e.data);
+    console.log('Message received from worker');
+  }
+
+  myWorker.postMessage([4, 6]);
 }
