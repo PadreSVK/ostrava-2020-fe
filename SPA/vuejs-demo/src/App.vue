17ec3bae57a02c93474cb74b27c57daca340cd75
Patrik Švikruha <patrik.svikruha@gmail.com>
2020-11-20 15:54:52 +0100

    Add vuex sample

--- SPA/vuejs-demo/src/App.vue
+++ SPA/vuejs-demo/src/App.vue
@@ -1,12 +1,12 @@
 <template>
   <v-app>
     <v-app-bar app color="primary" dark>
-      {{ $t("message") }}
+      {{ $t('message') }}
 
       <v-spacer />
 
       <v-btn @click="changeLocalization(langToChoose)" text>
-        <span class="mr-2"> {{ $t("changeLocalization") }}</span>
+        <span class="mr-2"> {{ $t('changeLocalization') }}</span>
         <v-icon>mdi-cached</v-icon>
       </v-btn>
 
@@ -17,28 +17,48 @@
     </v-app-bar>
 
     <v-main>
+      {{ myData }}
+      <v-btn @click="loadData"> Load DATA via store</v-btn>
+      {{ myModifiedData }}
       <router-view />
     </v-main>
   </v-app>
 </template>
 
 <script>
+import { mapGetters, mapState } from 'vuex'
+
 const langs = {
-  sk: "sk",
-  en: "en",
-};
+  sk: 'sk',
+  en: 'en'
+}
 
 export default {
-  name: "App",
+  name: 'App',
+  computed: {
+    ...mapState(['myData', 'myData2']),
+    // myData2: function () {
+    //   // ['myData', 'myData2'].map(i => this.computed[i] = this.$store.state[i])
+    //   return this.$store.state.myData
+    // },
+    ...mapGetters(['myModifiedData'])
+    // myModifiedData: function () {
+    //   return this.$store.getters.myModifiedData
+    // }
+  },
   data: () => ({
-    langToChoose: langs.sk,
+    langToChoose: langs.sk
   }),
   methods: {
     changeLocalization(langToChoose) {
-      this.$i18n.locale = langToChoose;
-      this.$vuetify.lang.current = langToChoose;
-      this.langToChoose = langToChoose == langs.sk ? langs.en : langs.sk;
+      this.$vuetify.lang.current = langToChoose // "sk"/"en"
+      this.$i18n.locale = langToChoose
+      
+      this.langToChoose = langToChoose == langs.sk ? langs.en : langs.sk
     },
-  },
-};
+    loadData() {
+      this.$store.dispatch('loadMyData')
+    }
+  }
+}
 </script>
--- SPA/vuejs-demo/src/store/index.js
+++ SPA/vuejs-demo/src/store/index.js
@@ -3,13 +3,32 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+
+export default new Vuex.Store(
+  {
+    state: {
+      myData: "my    Value",
+      myData2: "my  another  Value"
+    },
+    mutations: {
+      UPDATE_MY_VALUE(state, { myData }) {
+        state.myData = myData
+      }
+    },
+    actions: {
+      async loadMyData({ commit }) {
+        await delay(5000)
+        commit("UPDATE_MY_VALUE", { myData: "my changedData" })
+      }
+    },
+    getters: {
+      myModifiedData: (state) => `${state.myData} ++++ ${state.myData2}`
+    }
   }
-})
+
+)
